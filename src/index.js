d652d600b4fde8550aaa17711e891781e48269a4
Michael Primavera <pastasauce59@gmail.com>
2021-04-09 15:31:53 -0400

    Started submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+let form = document.querySelector('#create-task-form');
+
+//document.addEventListener("DOMContentLoaded", () => {
+  
+//});
+
+
+form.addEventListener('submit', function(evt){
+  evt.preventDefault();
+
+  debugger;
+  
+  let form2 = evt.target;
+  let input = form2.new-task-description
+  let whatUserTyped = input.value
+
+  let blankLi = document.createElement('li');
+
+  blankLi.classname = "new-task-description"
+  blankLi.innerText = whatUserTyped
+
+
+})

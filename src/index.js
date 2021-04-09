da92962a76f4e1910dfa10b34ed8d432eac6f03b
Ryan Andrews <rcandrews44@gmail.com>
2021-04-09 15:58:43 -0400

    Completed basic list submission

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 let form = document.querySelector('#create-task-form');
-
+let taskDiv = document.querySelector('ul#tasks')
 //document.addEventListener("DOMContentLoaded", () => {
   
 //});
@@ -9,10 +9,10 @@ let form = document.querySelector('#create-task-form');
 form.addEventListener('submit', function(evt){
   evt.preventDefault();
 
-  debugger;
+ 
   
   let form2 = evt.target;
-  let input = form2.new-task-description
+  let input = form2["new-task-description"]
   let whatUserTyped = input.value
 
   let blankLi = document.createElement('li');
@@ -20,5 +20,5 @@ form.addEventListener('submit', function(evt){
   blankLi.classname = "new-task-description"
   blankLi.innerText = whatUserTyped
 
-
+  taskDiv.append(blankLi)
 })
